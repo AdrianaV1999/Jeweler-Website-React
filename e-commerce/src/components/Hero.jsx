886e00a7d2bc57da9d718f4544a620a6876cee86
Adriana <vadriana258@gmail.com>
2025-03-08 14:30:33 +0100

    Policy + newsletter

--- e-commerce/src/components/Hero.jsx
+++ e-commerce/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col sm:flex-row border border-gray-330">
+    <div className="flex flex-col sm:flex-row border border-gray-400">
       <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
         <div className="text-[#414141]">
           <div className="flex items-center gap-2">
