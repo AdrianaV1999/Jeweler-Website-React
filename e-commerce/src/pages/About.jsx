c17583bb58b8f1380029f01162a0c5268a323bf0
Adriana <vadriana258@gmail.com>
2025-03-15 16:00:50 +0100

    changes on about + full contact

--- e-commerce/src/pages/About.jsx
+++ e-commerce/src/pages/About.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Title from "../components/Title";
+import Newsletter from "../components/Newsletter";
+
 const About = () => {
   return (
     <div>
       <div className="text-2xl text-center pt-8 border*t">
         <Title text1={"ABOUT"} text2={"US"} />
       </div>
-      <div className="my-10 flex glex-col md:flex-row gap-16">
+      <div className="my-10 flex flex-col md:flex-row gap-16">
         <img className="w-full md:max-w-[450px]" src="/about.jpg" alt="About" />
         <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
           <p>
@@ -44,6 +46,35 @@ const About = () => {
           <b>Jewelry tells a story—let us help you write yours.</b>
         </div>
       </div>
+      <div className="text-4xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US"} />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border border-gray-300 px-10 md:px py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-600">
+            Quality Assurance (QA) ensures that products meet high standards of
+            reliability, functionality, and user satisfaction through rigorous
+            testing and continuous improvement.
+          </p>
+        </div>
+        <div className="border border-gray-300 px-10 md:px py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className="text-gray-600">
+            Convenience is about making tasks easier, faster, and more
+            efficient, enhancing comfort and saving time in everyday life.
+          </p>
+        </div>
+        <div className="border border-gray-300 px-10 md:px py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className="text-gray-600">
+            Exceptional customer service goes beyond meeting expectations by
+            providing personalized, timely, and thoughtful support that creates
+            a lasting positive experience.
+          </p>
+        </div>
+      </div>
+      <Newsletter />
     </div>
   );
 };
